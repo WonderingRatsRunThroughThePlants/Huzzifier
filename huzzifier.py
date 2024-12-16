a59b850fd02016620c73e80d6c0260e5685f522d
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2024-12-15 22:29:04 -0600

    done

--- huzzifier.py
+++ huzzifier.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
-vowels = {'a','e','i','o','u'}
+vowels = {'','e','i','o','u','a'}
+newWord = ""
 
 def Huzzify(word):
     global newWord
@@ -11,19 +12,26 @@ def Huzzify(word):
                 splitWord.pop()
                 splitWord.append("uzz")
                 newWord = "".join(splitWord)
+                print(newWord)
     else:
-        print("Failed")
+        newWord = "Process failed."
 
 root = Tk()
 root.title("Huzzifier")
 root.geometry("400x400")
 root.config(bg="teal")
 
-frame = Frame(root)
-frame.grid(row=0, column=0)
-Label(frame, text="Huzzifier").pack(pady=10)
-userIn = Entry(frame)
-userIn.pack()
-Button(frame,text="Submit",command=lambda:Huzzify(userIn.get()))
+def ShowWord():
+    global newWord
+    frame = Frame(root)
+    frame.grid(row=0, column=0)
+    Label(frame, text="Huzzifier").pack(pady=10)
+    userIn = Entry(frame)
+    userIn.pack()
+    out = Label(frame, text=newWord)
+    Button(frame,text="Submit",command=lambda:[Huzzify(userIn.get()), out.config(text=newWord)]).pack()
+    out.pack()
+
+ShowWord()
 
 root.mainloop()
